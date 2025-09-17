2f955196b528648ebef42ea7ce6201b7471286d1
Viktor <vitygocanal@gmail.com>
2025-09-17 20:30:28 +0200

    fixed sidebar on mobile

--- src/components/MainPage/Header/Header.jsx
+++ src/components/MainPage/Header/Header.jsx
@@ -14,7 +14,13 @@ export default function Header(){
         <header className="header">
             <div className="navbar">
                 <div className="navbar__logo"><img src="./2.png" className="logo__img" alt="logo"/></div>
-                <GiHamburgerMenu onClick={() => setIsOpen(prev => !prev)} color="white" size='20%' />
+                <ul className='menu__link-list'>
+                        <li className='link-list__link'>Documentation</li>
+                        <li className='link-list__link'>Components</li>
+                        <li className='link-list__link'>Templates</li>
+                        <li className='link-list__link'>GitHub</li>
+                    </ul>
+                <GiHamburgerMenu className='navbar__burger-menu' onClick={() => setIsOpen(prev => !prev)} color="white" size='20%' />
                 </div>
                 <div className={`navbar__dropdown-menu ${isOpen ? 'open' : ''}`}>
                     <ul className='dropdown-menu__link-list'>
