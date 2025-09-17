b74921405cc2dbf1493625f8ef9bd56b0a5186c2
Viktor <vitygocanal@gmail.com>
2025-09-17 09:35:14 +0200

    add header dropdown

--- src/components/MainPage/Header/Header.jsx
+++ src/components/MainPage/Header/Header.jsx
@@ -1,10 +1,22 @@
 import './Header.css'
+import { GiHamburgerMenu } from "react-icons/gi";
+import { IoPartlySunny } from "react-icons/io5";
+import { IoPartlySunnyOutline } from "react-icons/io5";
+import React, {useState} from 'react'
+
 
 export default function Header(){
+
+    const [isOpen, setIsOpen] = useState(false)
+
+
     return(
         <header className="header">
             <div className="navbar">
-                <div className="navbar__logo"><img src="./logo1b.png" className="logo__img" alt="logo"/></div>
+                <div className="navbar__logo"><img src="./2.png" className="logo__img" alt="logo"/></div>
+                <GiHamburgerMenu onClick={() => setIsOpen(prev => !prev)} color="white" size='20%' />
+                </div>
+                <div className={`navbar__dropdown-menu ${isOpen ? 'open' : ''}`}>
 
             </div>
             
