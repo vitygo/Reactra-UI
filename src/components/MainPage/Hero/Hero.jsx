9a4f4f5219c93757b634747a0550f3705d76071f
Viktor <vitygocanal@gmail.com>
2025-09-17 23:09:33 +0200

    fix hero and header responsive

--- src/components/MainPage/Hero/Hero.jsx
+++ src/components/MainPage/Hero/Hero.jsx
@@ -5,7 +5,8 @@ export default function Hero(){
     return(
         <div className="hero-section">
             <div className="hero-section__hero-text">
-                <h1 className="hero-text__hero-title"><span>Build modern</span> React apps <span>faster</span> than ever.</h1>
+                <h1 className="hero-text__hero-title"><span>Build Modern</span> React Apps <span>Faster</span> Than Ever.</h1>
+                <button className='hero-text__cta-btn'>EXPLORE</button>
                 <p className="hero-text__hero-text">Save hours of development with ready-to-use, customizable components. Clean code, responsive design, and production-ready performance — all in one place.</p>
             </div>
             {/* <div className="hero-section__hero-img">
