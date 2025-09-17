693afc7ff958a2568cb245561d1b3dfac69b22ca
Viktor <vitygocanal@gmail.com>
2025-09-17 22:32:16 +0200

    add hero section

---
+++ src/components/MainPage/Hero/Hero.jsx
@@ -0,0 +1,16 @@
+import './Hero.css'
+
+
+export default function Hero(){
+    return(
+        <div className="hero-section">
+            <div className="hero-section__hero-text">
+                <h1 className="hero-text__hero-title"><span>Build modern</span> React apps <span>faster</span> than ever.</h1>
+                <p className="hero-text__hero-text">Save hours of development with ready-to-use, customizable components. Clean code, responsive design, and production-ready performance — all in one place.</p>
+            </div>
+            <div className="hero-section__hero-img">
+                <img src='../macbooki.png' className='hero-img__device-img' alt='laptop'/>
+            </div>
+        </div>
+    )
+}
--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,10 +1,14 @@
-
+import './MainPage.css'
+import Hero from './Hero/Hero'
 
 export default function MainPage(){
     return(
         <div className="main-page">
+            <Hero/>
+          
 
 
         </div>
     )
 }
+
