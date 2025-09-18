e7eb6d0f4240058b4070fc589730194b8977e737
Viktor <vitygocanal@gmail.com>
2025-09-18 23:17:34 +0200

    add new component to showcase

--- src/components/MainPage/ComponentsShowcase/ComponentsShowcase.jsx
+++ src/components/MainPage/ComponentsShowcase/ComponentsShowcase.jsx
@@ -11,7 +11,7 @@ const uiComponentsСategories = [
       subcategories: [
         { id: 101, name: "Buttons", posterComponent: <PrimaryButton/>},
         { id: 102, name: "Badges / Labels / Tags", posterComponent: <DefaultBadge/>},
-        { id: 103, name: "Icons" , posterComponent: 'none'},
+        { id: 103, name: "Icons" , posterComponent: <SearchIcon size='50px' color='white'/>},
         { id: 104, name: "Avatars / Profile Pictures", posterComponent: <AvatarWidthText/>},
         { id: 105, name: "Images / Thumbnails", posterComponent: 'none'}
       ]
@@ -184,6 +184,17 @@ export function SearchInput(){
             <input placeholder='Search React Component'/>
             <FaSearch size="20px"/>
         </div>
+    );
+   
+
+}
+
+export function SearchIcon({size, color}){
+    return(
+            <svg width={size} height={size} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M15.7955 15.8111L21 21M18 10.5C18 14.6421 14.6421 18 10.5 18C6.35786 18 3 14.6421 3 10.5C3 6.35786 6.35786 3 10.5 3C14.6421 3 18 6.35786 18 10.5Z" stroke={color} stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>
+     
     );
    
 
