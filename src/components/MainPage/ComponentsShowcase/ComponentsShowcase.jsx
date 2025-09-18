84c2f46365911b7524598856e5e15273a59e2431
Viktor <vitygocanal@gmail.com>
2025-09-18 15:37:54 +0200

    update components section and card

--- src/components/MainPage/ComponentsShowcase/ComponentsShowcase.jsx
+++ src/components/MainPage/ComponentsShowcase/ComponentsShowcase.jsx
@@ -7,76 +7,76 @@ const uiComponentsСategories = [
       id: 1,
       category: "Core UI Components",
       subcategories: [
-        { id: 101, name: "Buttons" },
-        { id: 102, name: "Badges / Labels / Tags" },
-        { id: 103, name: "Icons" },
-        { id: 104, name: "Avatars / Profile Pictures" },
-        { id: 105, name: "Images / Thumbnails" }
+        { id: 101, name: "Buttons", posterComponent: <PrimaryButton/>},
+        { id: 102, name: "Badges / Labels / Tags", posterComponent: <DefaultBadge/>},
+        { id: 103, name: "Icons" , posterComponent: 'none'},
+        { id: 104, name: "Avatars / Profile Pictures", posterComponent: 'none'},
+        { id: 105, name: "Images / Thumbnails", posterComponent: 'none'}
       ]
     },
     {
       id: 2,
       category: "Forms & Inputs",
       subcategories: [
-        { id: 201, name: "Input Fields / Text Inputs" },
-        { id: 202, name: "Textareas" },
-        { id: 203, name: "Checkboxes" },
-        { id: 204, name: "Radio Buttons" },
-        { id: 205, name: "Select / Dropdowns" },
-        { id: 206, name: "Switches / Toggles" },
-        { id: 207, name: "Sliders" },
-        { id: 208, name: "Date / Time Pickers" },
-        { id: 209, name: "Form Validation / Error Messages" }
+        { id: 201, name: "Input Fields / Text Inputs", posterComponent: 'none'},
+        { id: 202, name: "Textareas", posterComponent: 'none'},
+        { id: 203, name: "Checkboxes", posterComponent: 'none'},
+        { id: 204, name: "Radio Buttons", posterComponent: 'none'},
+        { id: 205, name: "Select / Dropdowns", posterComponent: 'none'},
+        { id: 206, name: "Switches / Toggles", posterComponent: 'none'},
+        { id: 207, name: "Sliders", posterComponent: 'none'},
+        { id: 208, name: "Date / Time Pickers", posterComponent: 'none'},
+        { id: 209, name: "Form Validation / Error Messages", posterComponent: 'none'}
       ]
     },
     {
       id: 3,
       category: "Navigation",
       subcategories: [
-        { id: 301, name: "Navbar / Header" },
-        { id: 302, name: "Sidebar / Drawer" },
-        { id: 303, name: "Breadcrumbs" },
-        { id: 304, name: "Pagination" },
-        { id: 305, name: "Tabs" },
-        { id: 306, name: "Menus / Dropdown Menus" },
-        { id: 307, name: "Steps / Progress Steps" }
+        { id: 301, name: "Navbar / Header", posterComponent: <BasicNavbar/>},
+        { id: 302, name: "Sidebar / Drawer", posterComponent: 'none'},
+        { id: 303, name: "Breadcrumbs", posterComponent: 'none'},
+        { id: 304, name: "Pagination", posterComponent: 'none'},
+        { id: 305, name: "Tabs" , posterComponent: 'none'},
+        { id: 306, name: "Menus / Dropdown Menus", posterComponent: 'none'},
+        { id: 307, name: "Steps / Progress Steps", posterComponent: 'none'}
       ]
     },
     {
       id: 4,
       category: "Layout & Display",
       subcategories: [
-        { id: 401, name: "Cards" },
-        { id: 402, name: "Modals / Dialogs" },
-        { id: 403, name: "Tooltips" },
-        { id: 404, name: "Popovers" },
-        { id: 405, name: "Accordions / Collapse" },
-        { id: 406, name: "Alerts / Notifications" },
-        { id: 407, name: "Toasts" },
-        { id: 408, name: "Tables / Data Grids" },
-        { id: 409, name: "Lists / List Groups" },
-        { id: 410, name: "Grids / Flex Layouts" }
+        { id: 401, name: "Cards", posterComponent: 'none'},
+        { id: 402, name: "Modals / Dialogs", posterComponent: 'none'},
+        { id: 403, name: "Tooltips", posterComponent: 'none'},
+        { id: 404, name: "Popovers", posterComponent: 'none'},
+        { id: 405, name: "Accordions / Collapse", posterComponent: 'none'},
+        { id: 406, name: "Alerts / Notifications", posterComponent: 'none'},
+        { id: 407, name: "Toasts", posterComponent: 'none'},
+        { id: 408, name: "Tables / Data Grids", posterComponent: 'none'},
+        { id: 409, name: "Lists / List Groups", posterComponent: 'none'},
+        { id: 410, name: "Grids / Flex Layouts", posterComponent: 'none'}
       ]
     },
     {
       id: 5,
       category: "Media & Content",
       subcategories: [
-        { id: 501, name: "Images / Media Objects" },
-        { id: 502, name: "Avatars" },
-        { id: 503, name: "Carousels / Sliders" },
-        { id: 504, name: "Badges" }
+        { id: 501, name: "Images / Media Objects", posterComponent: 'none'},
+        { id: 502, name: "Avatars", posterComponent: 'none'},
+        { id: 503, name: "Carousels / Sliders", posterComponent: 'none'},
+        { id: 504, name: "Badges", posterComponent: 'none'}
       ]
     },
     {
       id: 6,
       category: "Miscellaneous / Utility Components",
       subcategories: [
-        { id: 601, name: "Progress Bars" },
-        { id: 602, name: "Spinners / Loaders" },
-        { id: 603, name: "Tags / Chips" },
-        { id: 604, name: "Dividers / Separators" },
-        { id: 605, name: "Footers" }
+        { id: 601, name: "Progress Bars", posterComponent: 'none'},
+        { id: 602, name: "Spinners / Loaders", posterComponent: 'none'},
+        { id: 603, name: "Tags / Chips", posterComponent: 'none'},
+        { id: 604, name: "Dividers / Separators", posterComponent: 'none'},
+        { id: 605, name: "Footers", posterComponent: 'none'}
       ]
     }
   ];
@@ -93,7 +93,7 @@ export default function ComponentsShowcase(){
             <div className="components-showcase-section__components-grid">
                 {uiComponentsСategories.map(section => {
                    return section.subcategories.map(category => {
-                       return <ComponentCard key={category.id} name={category.name} />
+                       return <ComponentCard key={category.id} name={category.name} posterComponent={category.posterComponent}/>
                     })
 
                 })}
@@ -104,14 +104,14 @@ export default function ComponentsShowcase(){
     );
 }
 
-export function ComponentCard({name}){
+export function ComponentCard({name, posterComponent}){
     return(
         <div className="component-card">
             <div className="card__header">
                 <h4>{name}</h4>
             </div>
             <div className="card__body">
-               <PrimaryButton/>
+               {posterComponent}
             </div>
             
             </div>
@@ -126,3 +126,31 @@ export function PrimaryButton(){
    
 
 }
+
+export function DefaultBadge(){
+    return(
+        <div className="default__badge">Badge</div>
+    );
+   
+
+}
+
+export function BasicNavbar(){
+    return(
+        <div className="basic__navbar">
+            <div className="navbar__logo">
+                LOGO
+            </div>
+            <ul>
+                <li>link</li>
+                <li>link</li>
+                <li>link</li>
+                <li>link</li>
+            </ul>
+            
+        </div>
+    );
+   
+
+}
+
